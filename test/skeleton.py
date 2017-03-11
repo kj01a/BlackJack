0e8fa3426477f05e1f0208abe090a223f090048f
kj01a <john.prester01@gmail.com>
2017-03-10 20:14:20 -0600

    adding skeleton

---
+++ test/skeleton.py
@@ -0,0 +1,25 @@
+import random
+import json
+import sys
+
+
+class Card(object):
+    pass
+
+class Hand(Card):
+    pass
+
+class Dealer(Hand):
+    pass
+
+class Deck(Card):
+    pass
+
+class Boot(Deck):
+    pass
+
+class Player(object):
+    pass
+
+class Chips(object):
+    pass
