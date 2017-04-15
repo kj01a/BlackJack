0d9a9ac1db427f7a5d8fd0af8f6e741145b98e8c
kj01a <john.prester01@gmail.com>
2017-04-14 19:35:42 -0500

    Added pts skeleton

---
+++ test/pat_skelly.py
@@ -0,0 +1,33 @@
+class Card(object):
+	
+class Deck(Card):
+
+class Boot(Card):
+
+	def deal(self):
+	
+class Player(object):
+	
+	def Hit(self):
+	
+	def Stay(self):
+	
+	def Double_Down(self):
+	
+	def Split(self):
+	
+class Dealer(Player):
+
+	def Soft_Seventeen(self):
+
+class Bet(object):
+
+class One(Bet):
+
+class Five(Bet):
+
+class Ten(Bet):
+
+class Twenty_five(Bet):
+
+class Hundred(Bet):
