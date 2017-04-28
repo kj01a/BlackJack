2f3747c680c4fb8c92747bd36976a36e0c39b341
Patrick Tomei <Patrick Tomei>
2017-04-28 14:45:21 +0900

    Player Class Rough

---
+++ test/pts_testing/player.py
@@ -0,0 +1,33 @@
+class Player(object): # The player.
+	
+	def __init__(self, player_id, hand = None):
+		player_id.self = input("Set Player ID: ")
+	
+		player_id == hand[]
+		
+	def Split(self, hand): #split the hand into hand1 and hand2 only if cardx = cardy
+			if hand[0]= hand[1]:
+				hand.split.append(Player('hand_1', deck)
+				hand.split.append(Player('hand_2', deck)
+				for split in hand.split:
+					split.add_card(player.hand.pop(0))
+					split.add_card(the_boot.deal_card())
+					
+			else:
+				print "The dealer will not allow you to split."
+				pass
+					
+	def Hit(self, the_boot): # Draw another card. Possibly linked to Draw?
+		if hand < 21
+			print "The dealer adds another card to your hand."
+			the_boot.Hit #Draw a card from the boot
+				the_boot{2}	#Hand now contains 3 cards		
+			print "Your hand is now, %s." %(hand)
+		elif hand > 21
+			print "Sorry, but it appears that you fold."
+			
+		
+		else:
+			print "Congratulations!"
+	def Stay(self): # End hand
+		print "Players hand %s." %(hand)
