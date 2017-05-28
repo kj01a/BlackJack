9ce0c0eee1a8c2ff6b1666ea1036228ba2416fc3
kj01a <john.prester01@gmail.com>
2017-05-27 19:26:12 -0500

    changes to make card class more encapsulated

--- test/kjs_testing/boot2.py
+++ test/kjs_testing/boot2.py
@@ -8,19 +8,23 @@ I keep saying I'm going to write better variable names, and I never do...
 import json
 from random import shuffle
 
-class Card(object):
-    def __init__(self, card):
-        self.card = card[:-1]
-        self.card_value = card
+class Hand(object):
+    def __init__(self, card0, card1):
+        self.card0 = card0[:-1]
+        self.card1 = card1[:-1]
         
-    def set_card_values(self):
-        if self.card in "123456789":
-            self.card_value = int(self.card)
-        elif self.card in ["10", "J", "Q", "K"]:
-            self.card_value = 10
+    def set_card_value(self, card):
+        if card in "123456789":
+            card_value = int(card)
+        elif card in ["10", "J", "Q", "K"]:
+            card_value = 10
         else:
-            self.card_value =  11
-
+            card_value =  11
+        
+        return card_value
+        
+    def calc_hand_value(self):
+       self.hand_value =  self.set_card_value(self.card0) + self.set_card_value(self.card1)
     
 """
     Creates the boot that cards are dealt from.
@@ -52,25 +56,18 @@ def begin_game():
     d.create_boot(4)
     return d.shuffle_boot()
     
-def calc_hand_value(card0, card1):
-    hand_value = 0
-    first_card = Card(card0)
-    second_card = Card(card1)
-    
-    first_card.set_card_values()
-    second_card.set_card_values()
-    
-    hand_value = first_card.card_value + second_card.card_value
-    
-    return hand_value
+def get_hand_value(card0, card1):
+    h = Hand(card0, card1)
+    h.calc_hand_value()
+    return h.hand_value
     
 def engine(boot):
     print("The dealer deals your cards. They are: {0} {1}".format(boot[0], boot[1]))
-    print("And the value of the hand is: {0}".format(calc_hand_value(boot[0], boot[1])))
+    print("And the value of the hand is: {0}".format(get_hand_value(boot[0], boot[1])))
     boot.pop(0)
     boot.pop(0)
     print("The dealer deals her cards. They are: {0} {1}".format(boot[0], boot[1]))
-    print("And the value of the hand is: {0}".format(calc_hand_value(boot[0], boot[1])))
+    print("And the value of the hand is: {0}".format(get_hand_value(boot[0], boot[1])))
     boot.pop(0)
     boot.pop(0)
 
